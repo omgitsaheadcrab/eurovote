5b8f100b2a0b2f8880cb78607ccba597f14f917d
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2019-05-08 15:15:02 +0100

    Initial commit for barebones webapp with register/login/account functionality.

---
+++ eurovote/config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config:
+    SECRET_KEY = '5caec5a7d0cb10e6d24bf60b79b973a7'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
---
+++ eurovote/models.py
@@ -0,0 +1,29 @@
+from flask import current_app
+from eurovote import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default-user.png')
+    password = db.Column(db.String(60), nullable=False)
+    votes = db.relationship('Country', backref='voter', lazy=True)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    song = db.Column(db.Integer, nullable=False)
+    outfit = db.Column(db.Integer, nullable=False)
+    performance = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"Country('{self.name}', '{self.date_posted}')"
