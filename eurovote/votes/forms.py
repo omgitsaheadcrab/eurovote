806d80d915d1cd72c3951beb61a1ee4e470488a3
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2019-05-09 16:06:25 +0100

    Changed voting forms to radio buttons.

--- eurovote/votes/forms.py
+++ eurovote/votes/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
+from wtforms import IntegerField, SubmitField, RadioField
 from wtforms.validators import DataRequired
 
 class VoteForm(FlaskForm):
-   song = IntegerField('Song', validators=[DataRequired()])
-   outfit = IntegerField('Outfit', validators=[DataRequired()])
-   performance = IntegerField('Performance', validators=[DataRequired()])
+   song = RadioField('Song', coerce=int,
+                     choices=[(0,'0'),(1,'1'),(2,'2'),
+                              (3,'3'),(4,'4'),(5,'5')])
+   outfit = RadioField('Outfit', coerce=int,
+                       choices=[(0,'0'),(1,'1'),(2,'2'),
+                                (3,'3'),(4,'4'),(5,'5')])
+   performance = RadioField('Performance', coerce=int,
+                            choices=[(0,'0'),(1,'1'),(2,'2'),
+                                     (3,'3'),(4,'4'),(5,'5')])
+   
    submit = SubmitField('Submit')
